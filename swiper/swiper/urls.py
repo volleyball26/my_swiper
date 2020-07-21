44ce04cb31f33dadff3c311562bf3f5ce3071126
volleyball26 <54010676+volleyball26@users.noreply.github.com>
2020-07-21 20:08:30 +0800

    Revert "Vip"

--- swiper/swiper/urls.py
+++ swiper/swiper/urls.py
@@ -13,11 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
-
-from home import views
+from django.conf.urls import url
+from django.contrib import admin
 
 urlpatterns = [
-    url(r'^$', views.home),
-    url(r'^api/user/', include('user.urls', namespace='user'))
+    url(r'^admin/', admin.site.urls),
 ]
